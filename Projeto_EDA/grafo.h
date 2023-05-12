082e23e1e8eb742f91453cfadafc31305560d28a
joaovieirapt17 <joaovieirapt17@gmail.com>
2023-05-12 15:26:48 +0100

    Implementação da Estrutura dos Grafos

--- Projeto_EDA/grafo.h
+++ Projeto_EDA/grafo.h
@@ -1,3 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #pragma once
+
+#include "meio.h"
+#define TAM 50
+
+// Representa��o de um grafo orientado e pesado
+typedef struct aresta {
+	char geocode[TAM]; // geoc�digo what3words
+	float peso;
+} *ARESTA;
+
+typedef struct vertice {
+	char geocode[TAM]; // geoc�digo what3words
+	NODE* arestas;
+	NODE* meios; // Lista ligada com os c�digos dos meios de transporte existentes neste geoc�digo
+} *VERTICE;
 
