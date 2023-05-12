1464b90d9577b49bc6ab350d05cbfe2c14840d82
joaovieirapt17 <joaovieirapt17@gmail.com>
2023-05-12 15:30:56 +0100

    Implementação da função "add_vertice" e "find_vertice_by_geocode"

--- Projeto_EDA/grafo.c
+++ Projeto_EDA/grafo.c
@@ -1,4 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "grafo.h"
 
+
+int add_vertice(NODE** start, VERTICE* vertice) {
+	VERTICE* result = find_vertice_by_geocode(*start, vertice->geocode);
+	if (result != NULL) return -1;
+	return push(start, vertice, sizeof(VERTICE));
+}
+
+
+VERTICE* find_vertice_by_geocode(NODE* start, int geocode) {
+	NODE* aux = NULL;
+	VERTICE* vertice = NULL;
+
+	if (isEmpty(&start)) return NULL;
+
+	aux = start;
+	while (aux != NULL) {
+		vertice = (VERTICE*)aux->data;
+
+		if (vertice->geocode == geocode) {
+			return vertice;
+		}
+
+		aux = aux->next;
+	}
+
+	return NULL;
+}
 
--- Projeto_EDA/grafo.h
+++ Projeto_EDA/grafo.h
@@ -4,7 +4,7 @@
 #include "meio.h"
 #define TAM 50
 
-// Representa��o de um grafo orientado e pesado
+// Representação de um grafo orientado e pesado
 typedef struct aresta {
 	char geocode[TAM]; // geoc�digo what3words
 	float peso;
@@ -15,4 +15,9 @@ typedef struct vertice {
 	NODE* arestas;
 	NODE* meios; // Lista ligada com os c�digos dos meios de transporte existentes neste geoc�digo
 } *VERTICE;
+
+
+
+int add_vertice(NODE** start, VERTICE* vertice); 
+VERTICE* find_vertice_by_geocode(NODE* start, int geocode); 
 
